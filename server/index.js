3d8a155b919e3120a50525a26e9b8cbbc2d684e6
oliverpople <oliverpople@gmail.com>
2018-01-11 09:04:18 +0000

    Created Homepage, Navbar and footer components and imported them into the app.js component.

--- server/index.js
+++ server/index.js
@@ -2,14 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8080;
 const path = require('path');
-
 const app = express();
-app.use(bodyParser.json())
-app.use(express.static(`${_dirname}/../react-client/dist`));
 
-app.get('•', (req, res) => {
-    res.sendFile(path.resolve(`${_dirname}/../react-client/dist/index.html`));
+app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/../react-client/dist`));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(`${__dirname}/../react-client/dist/index.html`));
 });
+
+// app.use('/dist', express.static(path.join(__dirname, 'dist')));
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}!`);
