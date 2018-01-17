155ab1e815bc5ee5f23c7ed8c69bf7761fecbf06
oliverpople <oliverpople@gmail.com>
2018-01-17 11:38:32 +0000

    Installed Semantic-UI-React node package and use it to make navbar stick to top of page view.

--- react-client/src/Components/NavBar.js
+++ react-client/src/Components/NavBar.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Sticky } from 'semantic-ui-react'
 
 class NavBar extends Component {
   render() {
     return (
-      <header>
-        <ul id="headerButtons">
-          <li className="navButton"><Link to="">Home</Link></li>
-          <li className="navButton"><Link to="/#section2">About</Link></li>
-          <li className="navButton"><Link to="/#section3">Experience</Link></li>
-        </ul>
-      </header>
+      <Sticky >
+        <header>
+          <ul id="headerButtons">
+            <li className="navButton"><Link to="">Home</Link></li>
+            <li className="navButton"><Link to="/#section2">About</Link></li>
+            <li className="navButton"><Link to="/#section3">Experience</Link></li>
+          </ul>
+        </header>
+      </ Sticky>
     )
   }
 }
