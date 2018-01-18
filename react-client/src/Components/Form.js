ad3ea826f5da10e3699dbc037d6245f8d8eaee49
oliverpople <oliverpople@gmail.com>
2018-01-18 18:07:54 +0000

    Added submit button with event handler to Form component.  Prevented default behaviour of onSubmit (refreshing page and updating route) by passing the event and using the function e.preventDefault(); .

--- react-client/src/Components/Form.js
+++ react-client/src/Components/Form.js
@@ -16,6 +16,12 @@ export default class Form extends Component {
       });
     };
 
+    onSubmit = (e) => {
+      e.preventDefault();
+      console.log(this.state);
+    };
+
+
   render() {
     return (
       <form>
@@ -25,30 +31,36 @@ export default class Form extends Component {
         value={this.state.firstName}
         onChange={e => this.change(e)}
         />
+        <br />
         <input
         name="lastName"
         placeholder='LastName'
         value={this.state.lastName}
         onChange={e => this.change(e)}
         />
+        <br />
         <input
         name="userName"
         placeholder='UserName'
         value={this.state.userName}
         onChange={e => this.change(e)}
         />
+        <br />
         <input
         name="email"
         placeholder='Email'
         value={this.state.email}
         onChange={e => this.change(e)}
         />
+        <br />
         <input
         name="password"
         placeholder='Password'
         value={this.state.password}
         onChange={e => this.change(e)}
         />
+        <br />
+        <button onClick={e => this.onSubmit(e)}>Submit</button>
       </form>
     );
   }
