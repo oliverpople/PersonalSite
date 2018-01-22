76876ecc7c2331d476e1e031df17b220668e9fd3
oliverpople <oliverpople@gmail.com>
2018-01-22 12:15:24 +0000

    Installed react-vertical-timeline node package and began customising style.

--- react-client/src/Components/Experience.js
+++ react-client/src/Components/Experience.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
 import Helvetica from './helvetica.jpg';
+import {Timeline, TimelineEvent} from 'react-event-timeline';
+
 
 class Experience extends Component {
   render() {
     return (
-      <div className='title'>
-        Experience
-        CV CV CV
-        <img className='image' src={Helvetica} />
+      <div>
+        <div className='title'>Experience</div>
+        <Timeline>
+          <TimelineEvent title="John Doe sent a SMS"
+            icon={<i class="fa fa-briefcase" aria-hidden="true"></i>}
+            container="card"
+            iconColor="black"
+            bubbleStyle={{backgroundColor: "white"}}
+            cardHeaderStyle={{backgroundColor: "black", color: "white"}}
+          >
+            Details go here.
+          </TimelineEvent>
+          <TimelineEvent title="John Doe sent a SMS"
+            icon={<i class="fa fa-briefcase" aria-hidden="true"></i>}
+            container="card"
+            iconColor="black"
+            bubbleStyle={{backgroundColor: "white"}}
+            cardHeaderStyle={{backgroundColor: "black", color: "white"}}
+          >
+            Details go here.
+          </TimelineEvent>
+        </Timeline>
       </div>
     )
   }
 }
+
 export default Experience;
