81e52f20974263dd02ebd6bcdcdc4067d02b3ff3
oliverpople <oliverpople@gmail.com>
2018-01-26 18:25:16 +0000

    Added two more scripts to package.json so deploying on heroku requires little configuration.

---
+++ react-client/src/Components/ScrollArrow.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDelayRender from 'react-delay-render';
+
+class ScrollArrow extends Component {
+
+  render() {
+    return (
+      <div class="arrow bounce">
+        <a class="fa fa-arrow-down fa-2x" href="#"></a>
+      </div>
+    )
+  }
+
+}
+export default ReactDelayRender({ delay: 5000 })(ScrollArrow);
