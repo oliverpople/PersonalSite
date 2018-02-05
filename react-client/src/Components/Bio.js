b8b9cd0f7b4180003cfb003f3364d3671836590a
oliverpople <oliverpople@gmail.com>
2018-02-05 19:33:43 +0000

    updated Bio copy and moved email address to footer

--- react-client/src/Components/Bio.js
+++ react-client/src/Components/Bio.js
@@ -5,31 +5,29 @@ class Bio extends Component {
   render() {
     return (
       <div id='bioBoxContent'>
-        <a>Name: Oliver Pople</a>
-        <br></br>
-        <a>Email: oliverpople@gmail.com</a>
-        <h1>WHY TECH?</h1>
-        <br></br>
-        <a>When friends ask me what programming is, I say it's the closest thing
-        to being an Alchemist; you put phrases and commands together...and magic
-        happens! How cool is that?!</a>
+        <h1>I'M OLLIE!</h1>
           <br></br>
           <br></br>
-        <a>At least, that's how I felt when I was learning coding at night after work
-        and realised that I hadn't had this much fun in a very long time. I'd achieved
-        my early ambitions of a career in Advertising, now I was ready for a new adventure.</a>
-        <br></br>
-        <br></br>
-        <a>My vision isn't to change the world or take mankind to Mars - I know
-        there are already some very smart people working on that. What I do believe
-        is, in a world where 3D printers and self-driving cars have become a reality,
-         some things should be easier than they currently are. So I love building
-         things which make our everyday lives work like magic...or simply just for
-         the fun of it.</a>
-        <br></br>
-        <br></br>
-        <a>I'm curently looking for a full-stack role in Vancouver.  So if you're part of
-        a team who's looking for a Junior Developer please send me a message below.</a>
+        <a>I have just finished a software development boot camp in London, UK.
+        I am passionate about problem-solving and I believe that all code should
+        be well designed, intuitive, and flexible in the face of change.</a>
+          <br></br>
+          <br></br>
+        <a>I aim to maintain an up to date knowledge of the most current web trends
+        and best practices. Javascript is priority number one for me at the moment
+        and I am always trying to familiarise myself with the latest frameworks.
+        In fact, this very site is my first ever attempt at using React.js.</a>
+          <br></br>
+          <br></br>
+        <a>However, I’m not always glued to the screen - I also enjoy hiking,
+        martial arts, gymnastics, cooking, and taking as many trips abroad as
+        my finances will allow.</a>
+          <br></br>
+          <br></br>
+        <a>I am in the process of a big move and currently seeking new opportunities
+        in Vancouver, BC. If you think I'd be a good fit for your business or you'd
+        just like to have a chat about opportunities- use the Contact section below to
+        get in touch!</a>
         <br></br>
         <br></br>
       </div>
